ff304afa53ba2608aff4d7e344d22decad594d88
gs <gs@xyz.de>
2025-09-06 23:30:42 +0200

    last commit had wrong base_ref

--- open_meteo_ui/flutter_bootstrap.js
+++ open_meteo_ui/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2530937875"
+    serviceWorkerVersion: "3651656114"
   }
 });
